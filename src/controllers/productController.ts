bf6db0ef2ee2cc84cc92eadde8cd3ad5b11ba9d0
arianlb <83365444+arianlb@users.noreply.github.com>
2023-05-26 04:46:07 -0400

    Endpoints de Productos

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -49,6 +49,60 @@ export const getProduct = async (req: Request, res: Response) => {
     }
 }
 
+export const getProductAmount = async (req: Request, res: Response) => {
+    try {
+        const {
+            name,
+            price,
+            amountStock,
+            category,
+            tags,
+            description,
+            additionalInfo,
+            starts,
+            sku
+        } = req.query;
+
+        const query = { name, price, amountStock, category, tags, description, additionalInfo, starts, sku };
+        const total = await Product.countDocuments(query);
+        res.json(total);
+
+    } catch (error: any) {
+        res.status(500).json({ msg: error.message });
+    }   
+}
+
+export const getProductsSold = async (req: Request, res: Response) => {
+    try {
+        const products = await Product.find({ amountSold: { $gte: 1 } });
+        res.json(products);
+
+    } catch (error: any) {
+        res.status(500).json({ msg: error.message });
+    }
+}
+
+export const getFullProfit = async (req: Request, res: Response) => { 
+    try {
+        const products = await Product.find({ amountSold: { $gte: 1 } });
+        const profit = products.reduce((acc, product) => acc + (product.price * product.amountSold), 0);
+        res.json(profit);
+
+    } catch (error: any) {
+        res.status(500).json({ msg: error.message });
+    }
+}
+
+export const getProductsOutOfStock = async (req: Request, res: Response) => { 
+    try {
+        const products = await Product.find({ amountStock: 0 });
+        res.json(products);
+
+    } catch (error: any) {
+        res.status(500).json({ msg: error.message });
+    }
+}
+
 export const postProduct = async (req: Request, res: Response) => {
     try {
         const { name, price, amountStock, category, tags, description, additionalInfo, starts, sku } = req.body;
@@ -79,6 +133,27 @@ export const deleteProduct = async (req: Request, res: Response) => {
             return res.status(404).json({ msg: 'No existe el producto con el id: ' + req.params.id });
         }
 
+        res.json(product);
+
+    } catch (error: any) {
+        res.status(500).json({ msg: error.message });
+    }
+}
+
+export const sellProduct = async (req: Request, res: Response) => { 
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({ msg: 'No existe el producto con el id: ' + req.params.id });
+        }
+
+        if (product.amountStock === 0) {
+            return res.status(400).json({ msg: 'No hay stock del producto: ' + product.name });
+        }
+
+        product.amountStock -= 1;
+        product.amountSold += 1;
+        await product.save();
         res.json(product);
 
     } catch (error: any) {
